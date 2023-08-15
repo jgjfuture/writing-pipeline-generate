631f24353e28dfb8ed1e818099edcb26a613c909
jgjfuture <abovethechicken@outlook.jp>
2023-08-15 07:30:16 +0000

    remove language tag of code block

--- format.py
+++ format.py
@@ -3,4 +3,9 @@ import re
 def extract_code_blocks(text):
     pattern = r"```(.*?)```"
     matches = re.findall(pattern, text, re.DOTALL)
-    return matches[0] if matches else False
+    block_inner = matches[0] if matches else False
+    if not block_inner:
+        return False
+    # Remove first line "markdown" if present
+    pure_text = re.sub(r"^markdown\n", "", block_inner, flags=re.IGNORECASE)
+    return pure_text
