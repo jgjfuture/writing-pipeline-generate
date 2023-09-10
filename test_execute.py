eec60d6cb24d88ced086aec90cb89edf39150a00
jgjfuture <jgjfuture@outlook.jp>
2023-09-10 06:50:26 +0000

    fix: missing method in test script

--- test_execute.py
+++ test_execute.py
@@ -5,8 +5,7 @@ import format
 
 def main():
     api_key = os.getenv('OPENAI_API_KEY')
-    pubsub_topic_name = os.getenv('PUBSUB_TOPIC_NAME')
-    messages = gpt_generate.generate_messages(
+    messages = gpt_generate.generate_message_query_prompt(
         "書き出す\n5min\n- あしたの天気はあれらしいです？\n_どうにかじて騎麗なテキストを生成したい〇")
     chat_response = gpt_generate.gpt_generate(messages, api_key)
     if chat_response.choices[0].finish_reason != "stop":
